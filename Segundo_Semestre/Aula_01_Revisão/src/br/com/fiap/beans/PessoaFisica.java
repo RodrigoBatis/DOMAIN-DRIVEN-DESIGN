71333ac9bbd30550f1dee650553f22eba22e0d79
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-07 21:03:09 -0300

    Aula 01 Segundo Semestre -- Revisão

---
+++ Segundo_Semestre/Aula_01_Revisão/src/br/com/fiap/beans/PessoaFisica.java
@@ -0,0 +1,50 @@
+package br.com.fiap.beans;
+
+public class PessoaFisica extends Pessoa {
+	
+	private String cpf;
+
+	public PessoaFisica() {
+		super();
+	}
+
+	public PessoaFisica(String cpf) {
+		super();
+		this.cpf = cpf;
+	}
+	
+	public PessoaFisica(String nome, String email, int idade, double renda, String cpf) {
+		super(nome, email, idade, renda);
+		this.cpf = cpf;
+	}
+	
+	
+	
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	@Override
+	public String identificador() {
+		// TODO Auto-generated method stub
+		return "Pessoa Física";
+	}
+
+	@Override
+	public double pagarTaxa() {
+		// TODO Auto-generated method stub
+		double taxaRenda;
+		
+		if (renda > 2000) {
+			return taxaRenda = renda * 12/100;
+		}else {
+			taxaRenda = 0;
+			return taxaRenda;
+		}
+	}
+	
+}
---
+++ Segundo_Semestre/Aula_01_Revisão/src/br/com/fiap/main/App.java
@@ -0,0 +1,63 @@
+package br.com.fiap.main;
+
+import javax.swing.JOptionPane;
+
+import br.com.fiap.beans.PessoaFisica;
+import br.com.fiap.beans.PessoaJuridica;
+
+public class App {
+	
+	static String texto(String j) {
+		return JOptionPane.showInputDialog(j);
+	}
+
+	static double decimal(String j) {
+		return Double.parseDouble(JOptionPane.showInputDialog(j));
+	}
+	
+	static int inteiro(String j) {
+		return Integer.parseInt(JOptionPane.showInputDialog(j));
+	}
+	
+	public static void main(String[] args) {
+//		nome, email, idade, renda
+		PessoaJuridica objPJ = new PessoaJuridica(
+				texto("Digite seu nome: "),
+				texto("Digite seu email: "),
+				inteiro("Digite a idade da empresa: "),
+				decimal("Digite a renda da empresa: "),
+				texto("Digite o cnpj da empresa: ")
+		);
+		PessoaFisica objPF = new PessoaFisica(
+				texto("Digite seu nome: "),
+				texto("Digite seu email: "),
+				inteiro("Digite a sua idade: "),
+				decimal("Digite a sua renda: "),
+				texto("Digite o seu cpf: ")
+		);
+		
+		System.out.println(
+				  "--------------------------------------------"
+				+ "\n Identificação:   " + objPF.identificador() + ":"
+				+ "\n--------------------------------------------"
+				+ "\nNome Pessoa Física: " + objPF.getNome() 
+				+ "\nE-mail Pessoa Física: " + objPF.getEmail()
+				+ "\nIdade Pessoa Física: " + objPF.getIdade()
+				+ "\nRenda Pessoa Física: " + objPF.getRenda()
+				+ "\nCPF Pessoa Física: " + objPF.getCpf()
+				+ "\nTaxa a ser paga: " + objPF.pagarTaxa()
+				+ "\n--------------------------------------------"
+				+ "\n Identificação:   " + objPJ.identificador() + ":"
+				+ "\n--------------------------------------------"
+				+ "\nNome Pessoa Jurídica: " + objPJ.getNome() 
+				+ "\nE-mail Pessoa Jurídica: " + objPJ.getEmail()
+				+ "\nIdade Pessoa Jurídica: " + objPJ.getIdade()
+				+ "\nRenda Pessoa Jurídica: " + objPJ.getRenda()
+				+ "\nCNPJ Pessoa Jurídica: " + objPJ.getCnpj()
+				+ "\nTaxa a ser paga: " + objPF.pagarTaxa()
+				+ "\n--------------------------------------------"
+				
+		);
+	}
+
+}
